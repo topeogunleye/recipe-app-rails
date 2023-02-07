4e9055b61c9aa136c23f6a8ccb1eab47fdb6a488
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-07 22:18:52 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_143940) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_201045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "recipe_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -22,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_143940) do
     t.boolean "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_143940) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipe_foods", "recipes"
+  add_foreign_key "recipes", "users"
 end
