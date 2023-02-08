14bff3b25a8e63a7721558a3811a075c78eae642
adelguit <adeelguitoun@gmail.com>
2023-02-08 16:55:25 +0100

    Add inventories routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # root "foods#index"
-  resources :inventories, only: [:index, :show, :destroy]
+  resources :inventories, only: [:index, :show, :new, :create, :destroy]
 end
