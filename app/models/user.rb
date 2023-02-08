e09274b85fbac8f6f56c3efa40c382a73e3cf8d6
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-08 15:08:37 +0100

    Add changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :recipes, foreign_key: 'user_id'
-  has_many :foods, foreign_key: 'user_id'
   has_many :inventories
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :recipes
-  
-  get '/public_recipes', to: 'recipes#public_recipes'
-  get '/shopping_list/:id', to: 'recipes#shopping_list'
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  # root "foods#index"
   resources :inventories, only: [:index, :show, :destroy]
 end
