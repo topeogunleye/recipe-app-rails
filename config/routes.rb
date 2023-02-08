1552cbf885f11f7c66f4b544e9576ef8cbe8f7fb
adelguit <adeelguitoun@gmail.com>
2023-02-08 09:37:28 +0100

    Fix devise login_in and log_out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users
-
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # root "foods#index"
   resources :inventories, only: [:index, :show, :destroy]
 end
