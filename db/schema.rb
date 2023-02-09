4a70017ed9c1fd507913fab4dff1bb43cfb0ac1f
dlwub <dlwubi@gmail.com>
2023-02-09 08:06:31 +0300

    Add food list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_06_182722) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_093752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_06_182722) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "inventories", force: :cascade do |t|
     t.string "name"
