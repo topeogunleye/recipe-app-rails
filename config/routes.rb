62e75525a35dc741457e0568a8a2ba70a1fd855f
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-09 14:36:03 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :recipes
+  root to: 'recipes#public_recipes'
+  get 'shopping_list/:id', to: 'recipes#shopping_list'
+  get 'ingredients/new', to: 'recipes#new_ingredient'
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # root "foods#index"
-  resources :inventories, only: [:index, :show, :new, :create, :destroy]
-  resources :foods, only: [:new, :create, :index, :show, :destroy]
+  resources :inventories, only: [:index, :show, :destroy]
+
 end
