342ac578c45d9f261fa9e502653b799f01c3565c
adelguit <adeelguitoun@gmail.com>
2023-02-09 10:24:49 +0100

    Update the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_124416) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_091947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,26 +33,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_124416) do
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
-  create_table "recipe_foods", force: :cascade do |t|
+  create_table "inventory_foods", force: :cascade do |t|
     t.integer "quantity"
+    t.bigint "inventory_id", null: false
+    t.bigint "food_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "recipe_id"
-    t.bigint "food_id"
-    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
-    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
-  end
-
-  create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.integer "preparation_time"
-    t.integer "cooking_time"
-    t.text "description"
-    t.boolean "public"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_recipes_on_user_id"
+    t.index ["food_id"], name: "index_inventory_foods_on_food_id"
+    t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -73,4 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_124416) do
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
+  add_foreign_key "inventory_foods", "foods"
+  add_foreign_key "inventory_foods", "inventories"
 end
