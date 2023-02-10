00167c9ece71c3dfc0da2648ae9299bb738ae66b
adelguit <adeelguitoun@gmail.com>
2023-02-10 02:24:33 +0100

    Add the inventory_foods destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'ingredients/new', to: 'recipes#new_ingredient'
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   resources :inventories, only: [:index, :show, :new, :create, :destroy] do
-    resources :inventory_foods, only: [:new, :create]
+    resources :inventory_foods, only: [:new, :create, :destroy]
   end
   resources :foods, only: [:new, :create, :index, :show, :destroy]
 end
