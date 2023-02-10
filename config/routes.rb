af25977626aa8b1c98488885e1ef8e0457413a50
dlwub <dlwubi@gmail.com>
2023-02-10 10:44:06 +0300

    Add routes to public_recipes and general_shopping_list pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :recipes
+  get '/general_shopping_list', to: "recipes#general_shopping_list"
+  get '/public_recipes', to: "recipes#public_recipes"
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # root "foods#index"
   resources :inventories, only: [:index, :show, :new, :create, :destroy]
