ec72478512bda77dab215f4085c44f0546f61491
dlwub <dlwubi@gmail.com>
2023-02-10 17:23:05 +0300

    Modify params for authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     return unless user.present?
 
-    can :destroy, Food, id: user.id
+    can :destroy, Food, user_id: user.id
     can :destroy, Recipe, user_id: user.id
 
     # Define abilities for the user here. For example:
