d8db4c138723b6ecfd1b3303504e59507a6d9ce4
dlwub <dlwubi@gmail.com>
2023-02-10 11:28:14 +0300

    Modify ability.rb file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    return unless user.present?
     can :destroy, Food, id:user.id
     can :destroy, Recipe, user_id:user.id
 
--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_124416) do
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
+  create_table "inventory_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "inventory_id", null: false
+    t.bigint "food_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_inventory_foods_on_food_id"
+    t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
+  end
+
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
     t.datetime "created_at", null: false
@@ -70,6 +80,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_124416) do
 
   add_foreign_key "foods", "users"
   add_foreign_key "inventories", "users"
+  add_foreign_key "inventory_foods", "foods"
+  add_foreign_key "inventory_foods", "inventories"
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
