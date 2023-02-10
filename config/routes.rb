c7fabd0c22640c6a5369cbf2ddb4438617e6a4ab
dlwub <dlwubi@gmail.com>
2023-02-10 12:56:53 +0300

    Add public recipes controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  get 'public_recipe/index'
   resources :recipes
   get '/general_shopping_list', to: "recipes#general_shopping_list"
-  get '/public_recipes', to: "recipes#public_recipes"
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
-  # root "foods#index"
+  root "public_recipes#index"
   resources :inventories, only: [:index, :show, :new, :create, :destroy]
   resources :foods, only: [:new, :create, :index, :show, :destroy]
 end
