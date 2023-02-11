ecca1c286ccaae5eee4388e508dd9a09c2567123
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-11 01:04:05 +0100

    Update routes and add button for generating shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   get '/general_shopping_list', to: "recipes#general_shopping_list"
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   root "public_recipes#index"
-  resources :recipes do
+  resources :recipes, only: [:index, :show, :new, :create] do
     resources :recipe_foods, only: [:new, :create, :edit, :update, :destroy]
   end
   resources :inventories, only: [:index, :show, :new, :create, :destroy] do
     resources :inventory_foods, only: [:new, :create, :destroy]
   end
   resources :foods, only: [:new, :create, :index, :show, :destroy]
+  resources :shopping_list, only: [:index]
 end
