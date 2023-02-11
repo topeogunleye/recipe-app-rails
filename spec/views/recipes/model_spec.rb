dfd128822ae6dc15f572f2829b32f38a529cc032
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-11 02:08:29 +0100

    Add specs

---
+++ spec/views/recipes/model_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  before :each do
+    @user = User.new(name: 'Jerry', email: 'jerry@email.com', password: 'asdasd')
+    @user.save
+    @recipe = Recipe.new(name: 'Sushi', user_id: @user.id)
+    @recipe.save
+  end
+
+  it 'should be valid' do
+    expect(@recipe).to be_valid
+  end
+
+  it 'should be invalid without name' do
+    @recipe.name = nil
+    expect(@recipe).to_not be_valid
+  end
+
+  it 'should be invalid without user_id' do
+    @recipe.user_id = nil
+    expect(@recipe).to_not be_valid
+  end
+
+
+end
+
+  
