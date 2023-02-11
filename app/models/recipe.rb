57c15f524272dc039186f05727f2632a8a9b976a
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-11 15:10:47 +0100

    Fix public value for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-  validates :name, :preparation_time, :cooking_time, :description, :public, presence: true
+  validates :name, :preparation_time, :cooking_time, :description, presence: true
   belongs_to :user, foreign_key: 'user_id'
   has_many :recipe_foods, dependent: :destroy
   has_many :food, through: :recipe_foods
