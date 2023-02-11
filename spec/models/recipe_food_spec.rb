8e1f4ff7ade931baac7be4f6c20d39dfd6bc30e2
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-11 13:11:13 +0100

    Add validations to recipe food model and add tests for validations

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe RecipeFood, type: :model do
     it { is_expected.to belong_to(:recipe) }
     it { is_expected.to belong_to(:food) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_numericality_of(:quantity).only_integer.is_greater_than(0) }
+  end
 end
